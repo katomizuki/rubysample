d6082d57c18c5012cb555d374abed2462da9b421
Katopan <76249494+katomizuki@users.noreply.github.com>
2022-09-27 10:20:08 +0900

    一旦commit

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -3,28 +3,31 @@ class UserDecorator < ApplicationDecorator
 
   def end_job_button_disabled
     attendance = fetch_attendance()
+    if attendance.nil? 
+      return true
+    end 
     if attendance.is_finished_work?
       return true
     end
-    if attendance.is_working?
       return false
-    end
-    return true
   end
 
   def start_rest_button_disabled
     attendance = fetch_attendance()
-    if attendance.is_finished_work?
+    if attendance.nil? 
+      return true
+    end 
+    if attendance.is_finished_work? || attendance.is_rest? || attendance.is_finished_rest?
       return true
     end
-    if attendance.is_rest?
       return false
-    end
-      return true
   end
 
   def start_job_button_disabled
     attendance = fetch_attendance()
+    if attendance.nil? 
+      return false
+    end 
     if attendance.is_finished_work?
       return true
     end
@@ -36,6 +39,9 @@ class UserDecorator < ApplicationDecorator
 
   def end_rest_button_disabled
      attendance = fetch_attendance()
+     if attendance.nil? 
+      return true
+    end 
      if attendance.is_finished_work?
       return true
     end
@@ -48,6 +54,9 @@ class UserDecorator < ApplicationDecorator
 
   def work_status_alert_title
     attendance = fetch_attendance()
+    if attendance.nil? 
+      return "出勤前"
+    end 
     if attendance.is_working?
       return "勤務中だよ"
     end
@@ -61,7 +70,12 @@ class UserDecorator < ApplicationDecorator
   def fetch_attendance 
     user_id = object.id
     attendances = Attendance.where(user_id: user_id)
-    attendance = attendances.last
+    today = Date.today
+    month = today.month
+    day = today.day
+    year = today.year
+
+    attendance = attendances.filter{ |x| x.start_time.year == year && x.start_time.month == month && x.start_time.day == day }.last
     return attendance
   end
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,12 +3,16 @@ class Attendance < ApplicationRecord
 
   ## 勤務中かどうか
   def is_working?
-    !start_time.nil? && !is_rest && !is_finished_work
+    !start_time.nil? && !is_rest? && !is_finished_work?
   end
 
   ## 休憩中かどうか
   def is_rest?
     !rest_start_time.nil? && rest_end_time.nil?
+  end
+
+  def is_finished_rest?
+    !rest_end_time.nil?
   end
 
   ## その日を退勤しているかどうか
