440ca266f8f7832055ccbea96dc9e370f15b245c
Katopan <76249494+katomizuki@users.noreply.github.com>
2022-09-27 09:28:15 +0900

    commit

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,5 +1,18 @@
 class Attendance < ApplicationRecord
   belongs_to :user
 
-  
+  ## 勤務中かどうか
+  def is_working?
+    !start_time.nil? && !is_rest && !is_finished_work
+  end
+
+  ## 休憩中かどうか
+  def is_rest?
+    !rest_start_time.nil? && rest_end_time.nil?
+  end
+
+  ## その日を退勤しているかどうか
+  def is_finished_work?
+    !end_time.nil?
+  end
 end
